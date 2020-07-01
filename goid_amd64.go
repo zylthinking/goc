5589bf0d73dea3fc7520a760ea57e6677602ba33
zyl <zylthinking@users.noreply.github.com>
2020-07-01 16:52:35 +0800

    Update goid_amd64.go

--- goid_amd64.go
+++ goid_amd64.go
@@ -2,4 +2,29 @@
 
 package goc
 
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+var offset = func() int64 {
+	var offsetDict = map[string]int64{
+		"go1.4":  128,
+		"go1.5":  184,
+		"go1.6":  192,
+		"go1.7":  192,
+		"go1.8":  192,
+		"go1.9":  152,
+		"go1.10": 152,
+		"go1.11": 152,
+		"go1.12": 152,
+		"go1.13": 152,
+		"go1.14": 152,
+	}
+	ver := strings.Join(strings.Split(runtime.Version(), ".")[:2], ".")
+	return offsetDict[ver]
+}()
+
 func Goid() int64
