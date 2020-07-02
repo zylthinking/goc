23bcd71ace231c438432b680142c89e1ff20b050
zyl <zylthinking@users.noreply.github.com>
2020-07-02 09:13:29 +0800

    Update goid.go

--- goid.go
+++ goid.go
@@ -1,7 +1,25 @@
-// +build !amd64
-
 package goc
 
+import (
+	"reflect"
+	"unsafe"
+)
+
+//go:linkname __a runtime.convT2E
+func __a()
+func getg() unsafe.Pointer
+func getg_it() interface{}
+
+var g_goid_offset uintptr = func() uintptr {
+	it := getg_it()
+	if f, ok := reflect.TypeOf(it).FieldByName("goid"); ok {
+		return f.Offset
+	}
+	panic("can not find g.goid field")
+}()
+
 func Goid() int64 {
-    panic("only support amd64 now")
+	g := getg()
+	p := (*int64)(unsafe.Pointer(uintptr(g) + g_goid_offset))
+	return *p
 }
